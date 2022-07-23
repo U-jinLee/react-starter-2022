6b676a086f5da4cb13623de211106f744e4dd7bc
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-07-24 00:20:24 +0900

    Feat: props map 응용 방식

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-function Food({favorite}) {
-  return <h3> I Love {favorite}</h3>
+function Food({name, picture}) {
+  return (
+    <div>
+      <h3> I Love {name}</h3>
+      <img src= {picture}></img>
+    </div>
+  )
 }
+
+const foodLike = [
+  {
+    name: "kimchi",
+    image: "https://static.wtable.co.kr/image-resize/production/service/recipe/719/1x1/23757871-daf5-4a3d-b6e3-87b0a975f8d8.jpg",
+  },
+  {
+    name: "sushi",
+    image: "https://static.wtable.co.kr/image-resize/production/service/recipe/719/1x1/23757871-daf5-4a3d-b6e3-87b0a975f8d8.jpg",
+  },
+  {
+    name: "tendong",
+    image: "https://static.wtable.co.kr/image-resize/production/service/recipe/719/1x1/23757871-daf5-4a3d-b6e3-87b0a975f8d8.jpg",
+  },  
+];
 
 function App() {
   
   return (
     <div>
-      <Food favorite = "NadoJin"/>
-      <Food favorite = "Yoojin"/>
-      <div>Hello World</div>
+      {foodLike.map(dish => <Food name={dish.name} picture={dish.image} />)}
     </div> 
   )
 }
