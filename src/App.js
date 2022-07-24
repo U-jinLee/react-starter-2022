eca2240d43bcaf71e43d3b25ce36f643fe4e1c4b
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-07-24 17:12:10 +0900

    Feat: state에 사용할 js function 제작

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-class App extends React.Component{
-  
+class App extends React.Component {
+  state = {
+    count : 0,
+  }
+
+  add = () => {
+    console.log("add");
+  };
+
+  minus = () => {
+    console.log("minus");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>I'm a class component: {this.state.count}</h1>
+        <button onClick={this.add}>plus</button>
+        <button onClick={this.minus}>minus</button>
+      </div> 
+    )
+  }
 }
 
 export default App;
