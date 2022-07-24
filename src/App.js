cfd355eece3a05bfe2ec30efb1359f1499e19ee6
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-07-24 16:50:16 +0900

    Package: prop-types 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-function Food({name, picture}) {
+import PropTypes from 'prop-types'
+function Food({name, picture, rating}) {
   return (
     <div>
-      <h3> I Love {name}</h3>
+      <h3> I Love {name} {rating} / 5</h3>
       <img src= {picture} alt = {name}></img>
     </div>
   )
@@ -12,16 +13,19 @@ const foodLike = [
     id : 1,
     name: "kimchi",
     image: "https://static.wtable.co.kr/image-resize/production/service/recipe/719/1x1/23757871-daf5-4a3d-b6e3-87b0a975f8d8.jpg",
+    rating: 5,
   },
   {
     id : 2,
     name: "sushi",
     image: "https://static.wtable.co.kr/image-resize/production/service/recipe/719/1x1/23757871-daf5-4a3d-b6e3-87b0a975f8d8.jpg",
+    rating: 4.9,
   },
   {
     id : 3,
     name: "tendong",
     image: "https://static.wtable.co.kr/image-resize/production/service/recipe/719/1x1/23757871-daf5-4a3d-b6e3-87b0a975f8d8.jpg",
+    rating: 3.5,
   },  
 ];
 
@@ -30,9 +34,15 @@ function App() {
 
   return (
     <div>
-      {foodLike.map(dish => <Food key={dish.id} name = {dish.name} picture={dish.image}/>)}
+      {foodLike.map(dish => <Food key={dish.id} name = {dish.name} picture={dish.image} rating = {dish.rating}/>)}
     </div> 
   )
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
 }
 
 export default App;
