ead0a4370c7125354f31ab96a3080a08cd025cf4
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-07-24 16:57:35 +0900

    Feat: 클래스형 컴포넌트로 수정

--- src/App.js
+++ src/App.js
@@ -1,48 +1,7 @@
-import PropTypes from 'prop-types'
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h3> I Love {name} {rating} / 5</h3>
-      <img src= {picture} alt = {name}></img>
-    </div>
-  )
-}
+import React from "react";
 
-const foodLike = [
-  {
-    id : 1,
-    name: "kimchi",
-    image: "https://static.wtable.co.kr/image-resize/production/service/recipe/719/1x1/23757871-daf5-4a3d-b6e3-87b0a975f8d8.jpg",
-    rating: 5,
-  },
-  {
-    id : 2,
-    name: "sushi",
-    image: "https://static.wtable.co.kr/image-resize/production/service/recipe/719/1x1/23757871-daf5-4a3d-b6e3-87b0a975f8d8.jpg",
-    rating: 4.9,
-  },
-  {
-    id : 3,
-    name: "tendong",
-    image: "https://static.wtable.co.kr/image-resize/production/service/recipe/719/1x1/23757871-daf5-4a3d-b6e3-87b0a975f8d8.jpg",
-    rating: 3.5,
-  },  
-];
-
-
-function App() {
-
-  return (
-    <div>
-      {foodLike.map(dish => <Food key={dish.id} name = {dish.name} picture={dish.image} rating = {dish.rating}/>)}
-    </div> 
-  )
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+class App extends React.Component{
+  
 }
 
 export default App;
