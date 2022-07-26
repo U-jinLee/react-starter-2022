28af1073182cbea9a4fa92c1e2c9e3d4edb7d738
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-07-26 22:49:50 +0900

    Feat: get movies apis

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    isLoading : true
+    isLoading : true,
+    movies: []
+  };
+  
+  getMovies = async () => {
+    const {
+      data : {
+        data : {movies},
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies, isLoading : false});
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading : false});
-    }, 3000);
+    this.getMovies();
   }
 
   render() {
