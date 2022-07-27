b02ffefe87adf249f977cca770c07f11731f8b4e
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-07-27 22:37:07 +0900

    Feat: class -> className 변경

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({year, title, summary, poster}) {
+function Movie({year, title, summary, poster, genres}) {
     return (
-        <div class="movie">            
-            <div class = "movie__data">
+        <div className = "movie">            
+            <div className = "movie__data">
                 <img src={poster} alt={title} title={title}></img>
-                <h3 class = "movie__title">{title}</h3>
-                <div class = "movie__year">{year}</div>
-                <div class = "movie__summary">{summary}</div>
+                <h3 className = "movie__title">{title}</h3>
+                <div className = "movie__year">{year}</div>
+                <div className = "movie__summary">{summary}</div>
             </div>
         </div>
     );
@@ -19,6 +19,7 @@ Movie.prototypes = {
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
     poster : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
